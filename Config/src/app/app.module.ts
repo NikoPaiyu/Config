2b1a62077bf1b36c9a9549035b9696d9f97b8cae
NIkhil <nikhilpops@outlook.com>
2019-11-07 15:33:10 +0530

    routing changes commited

--- Config/src/app/app.module.ts
+++ Config/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './core/routing/app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/shared/header/header.component';
 import { DetailsComponent } from './components/main/quote/details/details.component';
--- Config/src/app/core/routing/app-routing.module.ts
+++ Config/src/app/core/routing/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ConfigHomeComponent } from 'src/app/components/config-home/config-home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path : '',
+  component : ConfigHomeComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
