34c09cf26bbdf9bb50432f7204fd9eda8c6ab969
ChandanaG <57387834+chandana2297@users.noreply.github.com>
2019-11-18 10:18:17 +0530

    Quote output

--- Config/src/app/components/main/quote/details/output/output.component.ts
+++ Config/src/app/components/main/quote/details/output/output.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-output',
   templateUrl: './output.component.html',
   styleUrls: ['./output.component.scss']
 })
+
+
+
 export class OutputComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  url = 'https://localhost:44369';
+ data;
+
+  outputType = [
+  {
+    'typeName' : 'pdf'
+  },
+    {
+    'typeName' : 'excel'  
+  }
+  ]
+
 
   ngOnInit() {
+    this.displaySomething();
+  }
+
+
+  displaySomething(){
+    this.http.get<any[]>(this.url + '/api/quoteoutput').subscribe(res => {
+     this.data = res[0];
+    },err => {
+      console.log(err);
+    })
   }
 
 }
+
+
+
